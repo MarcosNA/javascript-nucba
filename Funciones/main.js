d9ed5e7f3d5fe8ad501892a931bb568387ed2cb3
Marcos <nahuel_marcos@hotmail.com>
2021-07-20 21:40:21 -0300

    Funciones y validacion

--- Funciones/main.js
+++ Funciones/main.js
@@ -1,12 +1,12 @@
 function userData(name, surname, edad){
   if(edad < 18){
-  console.log("Usted se llama "+name+" "+surname+" y es menor de edad ");
+    return console.log("Usted se llama "+name+" "+surname+" y es menor de edad ");
   }
   else if(edad >= 18 && edad < 60){
-    console.log("Usted se llama "+name+ " "+surname+ " y es mayor de edad");
+    return console.log("Usted se llama "+name+ " "+surname+ " y es mayor de edad");
   }
   else if (edad >=60){
-    console.log("Usted se llama "+name+ " "+surname+ " y esta en la tercera edad");
+    return console.log("Usted se llama "+name+ " "+surname+ " y esta en la tercera edad");
   }
 
 }
